31b1b4f392c50135d63552bd319164436717a70e
futr3w4v <256798362+futurw4v@users.noreply.github.com>
2026-05-03 17:42:08 +0800

    feat(java.dart): 添加手动添加Java的逻辑

--- lib/pages/setting/java.dart
+++ lib/pages/setting/java.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:fml/constants.dart';
 import 'package:fml/function/java/java_service.dart';
+import 'package:fml/function/java/java_utils.dart';
 import 'package:fml/function/java/models/java_info.dart';
+import 'package:fml/function/java/models/java_runtime.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class JavaPage extends StatefulWidget {
   const JavaPage({super.key});
@@ -26,21 +32,37 @@ class JavaPageState extends State<JavaPage> {
 
         children: [
           // 大标题
-          Padding(
-            padding: const EdgeInsets.only(
-              left: kDefaultPadding / 2,
-              top: kDefaultPadding,
-              bottom: kDefaultPadding,
-            ),
-            child: Text(
-              '设备上的Java列表',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          Row(
+            // 使按钮组紧贴
+            mainAxisSize: MainAxisSize.min,
+
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: kDefaultPadding / 2,
+                  top: kDefaultPadding,
+                  bottom: kDefaultPadding,
+                ),
+
+                child: Text(
+                  '设备上的Java列表',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+
+              Spacer(),
+
+              IconButton(
+                icon: const Icon(Icons.add),
+                tooltip: '手动添加一个Java',
+                onPressed: () async => await _pickAndAddJavaRuntime(),
+              ),
+            ],
           ),
 
           _itemsPadding,
 
-          // 确保FutureBuilder占满剩余空间
+          // 确保ListView占满剩余空间
           Expanded(
             child: ListView.builder(
               itemCount: JavaService.javaRuntimes.length,
@@ -79,6 +101,163 @@ class JavaPageState extends State<JavaPage> {
           ),
         ],
       ),
+    );
+  }
+
+  ///
+  /// 显示FilePicker并根据选择的文件添加JavaRuntimes
+  ///
+  Future<void> _pickAndAddJavaRuntime() async {
+    // 打开FilePicker
+    final result = await FilePicker.platform.pickFiles(
+      dialogTitle: '选择Java路径',
+      type: FileType.custom,
+      allowedExtensions: Platform.isWindows ? ['exe'] : [],
+    );
+
+    if (!mounted) return;
+
+    // 未选择文件
+    if (result == null) {
+      showCustomDialog(
+        context: context,
+        title: '提示',
+
+        content: Text('未选择任何文件'),
+
+        actions: [
+          if (mounted)
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('关闭'),
+            ),
+        ],
+      );
+
+      return;
+    }
+
+    // 读取选择的文件的信息
+    final file = result.files.single;
+    final fileName = file.name.toLowerCase();
+    final validNames = Platform.isWindows
+        ? ['java.exe', 'javaw.exe']
+        : ['java', 'javaw'];
+
+    final exe = file.path!;
+    final info = await JavaUtils.probeJavaExecutable(exe);
+
+    // 选择的文件文件名合法
+    if (file.path != null && validNames.contains(fileName)) {
+      if (!mounted) return;
+
+      showCustomDialog(
+        context: context,
+        title: '正在添加Java',
+        content: SizedBox(
+          height: 80,
+          child: Center(child: CircularProgressIndicator()),
+        ),
+
+        barrierDismissible: false,
+      );
+
+      if (info != null) {
+        // 查重逻辑
+        final alreadyExists = JavaService.javaRuntimes.any(
+          (runtime) => runtime.executable == exe,
+        );
+
+        if (alreadyExists) {
+          Navigator.of(context).pop();
+
+          showCustomDialog(
+            context: context,
+            title: '提示',
+
+            content: Text('该Java已存在'),
+
+            actions: [
+              if (mounted)
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('关闭'),
+                ),
+            ],
+          );
+
+          return;
+        }
+
+        // 添加并写入SharedPreference
+        final isJdk = await JavaUtils.looksLikeJdk(exe);
+
+        JavaService.javaRuntimes.add(
+          JavaRuntime(info: info, executable: exe, isJdk: isJdk),
+        );
+
+        JavaService.writeRuntimesToPrefs(
+          await SharedPreferences.getInstance(),
+          JavaService.javaRuntimes,
+        );
+
+        Navigator.of(context).pop();
+
+        // 仅添加成功会有页面更新，故只在此处调用setState
+        setState(() {
+          showCustomDialog(
+            context: context,
+            title: '提示',
+            content: Text('添加 ${info.vendor} ${info.version} 成功！'),
+
+            actions: [
+              if (mounted)
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('关闭'),
+                ),
+            ],
+          );
+        });
+
+        return;
+      }
+    }
+
+    // 最后分支，选择的文件文件名不合法等情况
+    if (!mounted) return;
+
+    Navigator.of(context).maybePop();
+
+    showCustomDialog(
+      context: context,
+      title: '提示',
+      content: Text('请选择正确的Java可执行文件'),
+
+      actions: [
+        TextButton(
+          onPressed: () => {if (mounted) Navigator.of(context).pop()},
+          child: Text('关闭'),
+        ),
+      ],
+    );
+  }
+
+  ///
+  /// 显示一个自定义的AlertDialog
+  ///
+  void showCustomDialog({
+    required BuildContext context,
+    required String title,
+    Widget? content,
+    bool barrierDismissible = true,
+    List<Widget>? actions,
+  }) {
+    showDialog(
+      context: context,
+      barrierDismissible: barrierDismissible,
+      builder: (_) =>
+          AlertDialog(title: Text(title), content: content, actions: actions),
     );
   }
 
